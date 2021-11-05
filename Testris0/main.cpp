e0c72bc0315d4282c04cbf29036102edfe0763d6
usuyn <usuyn000@gmail.com>
2021-11-06 05:53:23 +0900

    Create(main) : cpp include main function

---
+++ Testris0/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "GameUI.h"
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	GameUI GUI;
+	GUI.SetCursorView(0);
+	GUI.show_logo();
+	return 0;
+}
